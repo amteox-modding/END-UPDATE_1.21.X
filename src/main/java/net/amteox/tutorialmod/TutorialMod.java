e473ed7563ed932044100d03e2176ead5b7969f6
amteox <mateuszgracyasz@gmail.com>
2024-12-23 16:29:45 +0100

    yep?

---
+++ src/main/java/net/amteox/tutorialmod/TutorialMod.java
@@ -0,0 +1,23 @@
+package net.amteox.tutorialmod;
+
+import net.amteox.tutorialmod.block.ModBlock;
+import net.amteox.tutorialmod.item.ModItemGroups;
+import net.amteox.tutorialmod.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
+
+		ModBlock.registerModBlocks();
+	}
+}
---
+++ src/main/java/net/amteox/tutorialmod/TutorialModDataGenerator.java
@@ -0,0 +1,25 @@
+package net.amteox.tutorialmod;
+
+import net.amteox.tutorialmod.datagen.*;
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
+
+public class TutorialModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
+
+
+
+
+	}
+
+}
